04eafb6f495530cd0ce6dd54ed492cd909835f33
kopiflores <akuninternet.fs@gmail.com>
2025-02-14 00:07:06 +0800

    update note handler

--- models/note.go
+++ models/note.go
@@ -1,31 +1,3 @@
-<<<<<<< HEAD
-// models/note.go
-package models
-
-import (
-	"github.com/jinzhu/gorm"
-	_ "github.com/mattn/go-sqlite3" 
-)
-
-// Note struct defines the note entity
-type Note struct {
-	ID      uint   `json:"id"`
-	Title   string `json:"title"`
-	Content string `json:"content"`
-}
-
-// Initialize DB connection (optional)
-var DB *gorm.DB
-
-func InitializeDB() {
-	var err error
-	DB, err = gorm.Open("sqlite3", "./notes.db")
-	if err != nil {
-		panic("failed to connect database")
-	}
-	DB.AutoMigrate(&Note{})
-}
-=======
 package models
 
 import (
@@ -59,4 +31,3 @@ func InitializeDB() {
 	DB.AutoMigrate(&Note{})
 	DB.AutoMigrate(&Agenda{})
 }
->>>>>>> c6ab4a0 (update note handler)
--- routes/routes.go
+++ routes/routes.go
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-// routes/routes.go
-package routes
-
-import (
-	"github.com/gorilla/mux"
-	"golang-rest-api-notes/handlers"
-)
-
-// SetupRouter initializes routes
-func SetupRouter() *mux.Router {
-	r := mux.NewRouter()
-	r.HandleFunc("/notes", handlers.GetNotes).Methods("GET")
-	r.HandleFunc("/notes", handlers.CreateNote).Methods("POST")
-	r.HandleFunc("/notes/{id}", handlers.GetNoteByID).Methods("GET")
-	r.HandleFunc("/notes/{id}", handlers.UpdateNote).Methods("PUT")
-	r.HandleFunc("/notes/{id}", handlers.DeleteNote).Methods("DELETE")
-	return r
-}
-=======
 package routes
 
 import (
@@ -36,4 +16,3 @@ func SetupRouter() *mux.Router {
 
 	return router
 }
->>>>>>> c6ab4a0 (update note handler)
